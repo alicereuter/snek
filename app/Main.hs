37b3c3f081218ba14681e68a4e6725026783f815
alicereuter <42192226+alicereuter@users.noreply.github.com>
2019-03-31 19:55:28 -0500

    screen drawing

--- app/Main.hs
+++ app/Main.hs
@@ -1,21 +1,38 @@
 module Main where
 
 import UI.NCurses
+import Control.Concurrent
+import Control.Monad.IO.Class
+
 
 data Direction = Up | Down | Left | Right
-data Segment = Segment Int Int Direction
+data Segment = Segment Integer Integer Direction
 data Snake = Snake [Segment]
+
+
+start = Snake [Segment 5 12 Up, Segment 6 12 Up]
+
+next (Snake s) = Snake $ map (\(Segment x y d) -> Segment (x+1) y d) s
 
 main :: IO ()
 main = runCurses $ do
     setEcho False
     w <- defaultWindow
-    updateWindow w $ do
+    loop w (\ev -> ev == EventCharacter 'q' || ev == EventCharacter 'Q') start
+
+loop w p s = do
+
+  ev <- getEvent w Nothing
+  updateWindow w $ do
+      clear
       drawGrid
-      drawSnake
-        
-    render
-    waitFor w (\ev -> ev == EventCharacter 'q' || ev == EventCharacter 'Q')
+      drawSnake start
+
+  render
+  liftIO $ threadDelay 1000000
+  case ev of
+    Nothing -> loop w p s
+    Just ev' -> if p ev' then loop w p $ next s else loop w p s
 
 
 drawGrid = do
@@ -31,15 +48,11 @@ drawGrid = do
           moveCursor x 59
           drawString "#"
           
-drawSnake = do
-  moveCursor 2 30
-  drawString "O"
+drawSnake (Snake segs) = do
+  drawSeg "O" $ head segs
+  mapM (drawSeg "o") $ tail segs
+  pure ()
+  where drawSeg c (Segment x y _) = do
+          moveCursor x y
+          drawString c
 
-  
-waitFor :: Window -> (Event -> Bool) -> Curses ()
-waitFor w p = loop where
-    loop = do
-        ev <- getEvent w Nothing
-        case ev of
-            Nothing -> loop
-            Just ev' -> if p ev' then return () else loop
